10fb17b56335fa7e511c13e7279f6a834399c1a0
lucas Franca <lucasgamesdark@gmail.com>
2022-10-08 18:43:14 -0300

    Data fetching

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,48 +1,66 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
-import * as yup from 'yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMutation } from 'react-query'
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
-import Image from "next/image";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+
+type CreateUserFormData = {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+};
 
 const createUserFormSchema = yup.object().shape({
-    name: yup.string().required('Nome obrigatória'),
+    name: yup.string().required('Nome obrigatório'),
     email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
     password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
     password_confirmation: yup.string().oneOf([
-        null,
-        yup.ref('password')
-    ], 'As senhas precisam ser iguais'),
+        null, yup.ref('password')
+    ], 'As senhas precisam ser iguais')
 })
 
 export default function CreateUser() {
+    const router = useRouter()
 
-    const { register, handleSubmit, formState } = useForm({
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        })
+
+        return response.data.user;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users')
+        }
+    });
+
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
 
-    const { errors } = formState;
+    const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+        await createUser.mutateAsync(values);
 
-    const handleCreateUser: SubmitHandler<FieldValues> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        console.log(values)
+        router.push('/users')
     }
-
 
     return (
         <Box>
             <Header />
-            <Flex
-                w="100%"
-                my="6"
-                maxWidth={1480}
-                mx="auto"
-                px="6"
-            >
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
                 <Box
@@ -52,18 +70,18 @@ export default function CreateUser() {
                     bg="gray.800"
                     p={["6", "8"]}
                     onSubmit={handleSubmit(handleCreateUser)}
+                    noValidate
                 >
                     <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
-                    
+
                     <Divider my="6" borderColor="gray.700" />
 
                     <VStack spacing="8">
                         <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input
-                            type="text"
-                             label="Nome completo"
-                            error={errors.name}
-                            {...register('name')}
+                                label="Nome completo"
+                                error={errors.name}
+                                {...register('name')}
                             />
                             <Input
                                 type="email"
@@ -91,15 +109,14 @@ export default function CreateUser() {
 
                     <Flex mt="8" justify="flex-end">
                         <HStack spacing="4">
-                            <Link href='/users' passHref>
+                            <Link href="/users" passHref>
                                 <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
                             </Link>
-
                             <Button
-                                type="submit" 
+                                type="submit"
                                 colorScheme="pink"
-                                isLoading={formState.isSubmitting}
-                                >
+                                isLoading={isSubmitting}
+                            >
                                 Salvar
                             </Button>
                         </HStack>
@@ -107,5 +124,5 @@ export default function CreateUser() {
                 </Box>
             </Flex>
         </Box>
-    )
+    );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,103 +1,113 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import NextLink from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Link, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList() {
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page)
 
     const isWideVersion = useBreakpointValue({
         base: false,
-        lg: true
+        lg: true,
     })
+
+    async function handlePrefetchUser(userId: string) {
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data;
+        }, {
+            staleTime: 1000 * 60 * 10, // 10 minutes
+        })
+    }
+
     return (
         <Box>
             <Header />
-            <Flex
-                w="100%"
-                my="6"
-                maxWidth={1480}
-                mx="auto"
-                px="6"
-            >
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        
-                        <Link href='/users/create' passHref>
-                        <Button 
-                        as="a" 
-                        size="sm" 
-                        fontSize="sm" 
-                        colorScheme="pink"
-                        leftIcon={<Icon as={RiAddLine} fontSize="28"/>}
-                        >
-                            Criar novo 
-                        </Button>
-                        </Link>
-                       
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
+
+                        <NextLink href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar novo
+                            </Button>
+                        </NextLink>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4","4","6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink"/>
-                                </Th>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.users.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                        </Link>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.created_at}</Td>}
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
 
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width="8"></Th>
-                            </Tr>
-                        </Thead>
-
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4","4","6"]}>
-                                <Checkbox colorScheme="pink"/>
-                                </Td>
-
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Lucas Pereira</Text>
-                                        <Text fontSize="small" color="gray.300">lucasgamesdark@gmail.com</Text>
-                                    </Box>
-                                </Td>
-
-                                {isWideVersion && <Td>04 de Abril, 2022</Td>}
-
-                                <Td>
-
-                                    {!isWideVersion &&
-                                     <Button 
-                                     as="a" 
-                                     size="sm" 
-                                     fontSize="sm" 
-                                     colorScheme="purple"
-                                     ><Icon as={RiPencilLine}/>
-                                     </Button>}
-
-                               {isWideVersion && <Button 
-                        as="a" 
-                        size="sm" 
-                        fontSize="sm" 
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine}/>}
-                        >
-                            Editar
-                        </Button>}
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination/>
-
+                            <Pagination
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
+                        </>
+                    )}
                 </Box>
-                </Flex>
+            </Flex>
         </Box>
-    )
+    );
 }
